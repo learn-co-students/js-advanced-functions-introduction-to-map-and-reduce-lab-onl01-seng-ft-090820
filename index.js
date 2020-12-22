c587581b010fd9a68f65f7ac971980025791ece3
HeartyRhombus <kaylahmg@gmail.com>
2020-12-22 13:26:48 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Your code here
+function mapToNegativize(sourceArray){
+    return sourceArray.map(elem => elem * -1 )
+}
+// function mapToNegativize(sourceArray){
+//     let newArray = []
+//     sourceArray.forEach( elem => {
+//         newArray.push(elem * -1)
+//     })
+//     return newArray
+// }
+
+function mapToNoChange(sourceArray){
+    return sourceArray.map( elem => elem)
+}
+
+function mapToDouble(sourceArray){
+    return sourceArray.map( elem => elem * 2 )
+}
+
+function mapToSquare(sourceArray){
+    return sourceArray.map( elem => elem ** 2 )
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+    return sourceArray.reduce((total, elem) => {
+        return elem + total
+    }, startingPoint)
+}
+
+function reduceToAllTrue(sourceArray){
+    const bln = (accum, currValue) => {
+        if (!!accum === true && !!currValue === true){
+            return true
+        } else {
+            return false
+        }
+    }
+    return sourceArray.reduce(bln, true)
+}
+
+function reduceToAnyTrue(sourceArray){
+    const bln = (accum, currValue) => {
+        if (accum === true){
+            return true
+        } else {
+            return !!currValue
+        }
+    }
+    return sourceArray.reduce(bln, false)
+}
