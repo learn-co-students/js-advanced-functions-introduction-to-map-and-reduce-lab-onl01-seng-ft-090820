e9db8c06aa5c61d92eb043a19f01c976eeda8ed3
QueenTesa Fuggett <qfuggett@umich.edu>
2021-01-03 14:25:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+function mapToNegativize(array){
+    return array.map(x => x * -1);
+}
+
+function mapToNoChange(array){
+    return array
+}
+
+function mapToDouble(array){
+    return array.map(x => x * 2);
+}
+
+function mapToSquare(array){
+    return array.map(x => x ** 2);
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+    let reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return sourceArray.reduce(reducer, startingPoint);
+}
+
+function reduceToAllTrue(sourceArray){
+    const reducer = (accumulator, currentValue) => {  
+        if(!!accumulator == true && !!currentValue == true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return sourceArray.reduce(reducer, true)
+}
+
+function reduceToAnyTrue(sourceArray){
+    const reducer = (accumulator, currentValue) => {  
+        if(accumulator == true){
+            return true
+        } else {
+            return !!currentValue
+        } 
+    }
+    return sourceArray.reduce(reducer, false)
+}
